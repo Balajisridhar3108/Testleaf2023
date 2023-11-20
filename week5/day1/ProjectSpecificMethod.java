5088b442fd307c9f8c8f14daa15154ad9dc17ae7
Balaji <92989836+Balajisridhar3108@users.noreply.github.com>
2023-11-20 21:01:32 +0530

    Add files via upload

---
+++ week5/day1/ProjectSpecificMethod.java
@@ -0,0 +1,33 @@
+package week5.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class ProjectSpecificMethod {
+	public ChromeDriver driver;
+	@Test
+    @BeforeMethod
+	public  void precondition() {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+
+	}
+    @AfterMethod
+    public void postcondition() {
+    	driver.close();
+    }
+    
+
+}

cb012588d19af9e66c969a0f5bee2c9c8f01371a
Balaji <92989836+Balajisridhar3108@users.noreply.github.com>
2023-11-22 17:55:43 +0530

    Add files via upload

---
+++ week5/day2/MergeLead.java
@@ -0,0 +1,47 @@
+package week5.day2;
+
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class MergeLead {
+	@Test
+
+	public  void mergelead() throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Merge Leads")).click();
+		driver.findElement(By.xpath("//img[@alt='Lookup']")).click();
+		Set<String> allWindows = driver.getWindowHandles();
+		List<String> allhandles = new ArrayList<String>(allWindows);
+		driver.switchTo().window(allhandles.get(1));
+		driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("gopi");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		driver.close();
+		
+		
+	}
+}
+
+
+
+
+
+
